158364f969b91c9a0e31c557c3278205d1cca439
Rene Hopf <renehopf@mac.com>
2013-12-19 01:47:38 +0100

    ugly hacked term with feedback, but it works

--- term/graph.cpp
+++ term/graph.cpp
@@ -3,6 +3,12 @@
 #include <math.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <string.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #include <GLFW/glfw3.h>
 
@@ -11,8 +17,9 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "shader_utils.h"
+#include "stlink-common.h"
 
-#define LEN 100
+#define LEN 500
 
 GLuint program;
 GLint attribute_coord2d;
@@ -34,6 +41,139 @@ GLFWwindow* window;
 
 const int border = 10;
 const int ticksize = 10;
+
+#define STLINKY_MAGIC 0xDEADF00D
+
+#define READ_UINT32_LE(buf)  ((uint32_t) (   buf[0]         \
+                                           | buf[1] <<  8   \
+                                           | buf[2] << 16   \
+                                           | buf[3] << 24))
+
+typedef struct {
+        stlink_t *sl;
+        uint32_t off;
+        size_t bufsize;
+}stlinky;
+
+/* Detects stlinky in RAM, returns handler */
+stlinky*  stlinky_detect(stlink_t* sl)
+{
+        static const uint32_t sram_base = 0x20000000;
+        stlinky* st = (stlinky*)malloc(sizeof(stlinky));
+        int multiple=0;
+        st->sl = sl;
+        printf("sram: 0x%x bytes @ 0x%zx\n", sl->sram_base, sl->sram_size);
+        uint32_t off;
+        for (off = 0; off < sl->sram_size; off += 4) {
+                stlink_read_mem32(sl, sram_base + off, 4);
+                if (STLINKY_MAGIC == READ_UINT32_LE(sl->q_buf))
+                {
+                        if (multiple > 0)
+                        printf("WARNING: another ");
+                        printf("stlinky detected at 0x%x\n", sram_base + off);
+                        st->off = sram_base + off;
+                        stlink_read_mem32(sl, st->off + 4, 4);
+                        st->bufsize = (size_t) *(unsigned char*) sl->q_buf;
+                        printf("stlinky buffer size 0x%zu \n", st->bufsize);
+                        multiple++;
+                }
+        }
+        if (multiple > 0) {
+                if (multiple > 1) {
+                        printf("Using last stlinky structure detected\n");
+                }
+                return st;
+        }
+        return NULL;
+}
+
+int stlinky_canrx(stlinky *st)
+{
+        stlink_read_mem32(st->sl, st->off+4, 4);
+        unsigned char tx = (unsigned char) st->sl->q_buf[1];
+        return (int) tx;
+}
+
+size_t stlinky_rx(stlinky *st, char* buffer)
+{
+        unsigned char tx = 0;
+        while(tx == 0) {
+                stlink_read_mem32(st->sl, st->off+4, 4);
+                tx = (unsigned char) st->sl->q_buf[1];
+        }
+        size_t rs = tx + (4 - (tx % 4)); /* voodoo */
+        stlink_read_mem32(st->sl, st->off+8, rs);
+        memcpy(buffer, st->sl->q_buf, (size_t) tx);
+        *st->sl->q_buf=0x0;
+        stlink_write_mem8(st->sl, st->off+5, 1);
+        return (size_t) tx;
+}
+
+size_t stlinky_tx(stlinky *st, char* buffer, size_t sz)
+{
+        unsigned char rx = 1;
+        while(rx != 0) {
+                stlink_read_mem32(st->sl, st->off+4, 4);
+                rx = (unsigned char) st->sl->q_buf[2];
+        }
+        memcpy(st->sl->q_buf, buffer, sz);
+        size_t rs = sz + (4 - (sz % 4)); /* voodoo */
+        stlink_write_mem32(st->sl, st->off+8+st->bufsize, rs);
+        *st->sl->q_buf=(unsigned char) sz;
+        stlink_write_mem8(st->sl, st->off+6, 1);
+        return (size_t) rx;
+}
+
+int kbhit()
+{
+        struct timeval tv;
+        fd_set fds;
+        tv.tv_sec = 0;
+        tv.tv_usec = 0;
+        FD_ZERO(&fds);
+        FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+        select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+        return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+void nonblock(int state)
+{
+        struct termios ttystate;
+
+        //get the terminal state
+        tcgetattr(STDIN_FILENO, &ttystate);
+
+        if (state==1)
+        {
+                //turn off canonical mode
+                ttystate.c_lflag &= ~ICANON;
+                ttystate.c_lflag &= ~ECHO;
+                //minimum of number input read.
+                ttystate.c_cc[VMIN] = 1;
+        }
+        else if (state==0)
+        {
+                //turn on canonical mode
+                ttystate.c_lflag |= ICANON | ECHO;
+        }
+        //set the terminal attributes.
+        tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+
+}
+
+static int keep_running = 1;
+static int sigcount=0;
+void cleanup(int dummy)
+{
+        (void) dummy;
+        sigcount++;
+        keep_running = 0;
+        printf("\n\nGot a signal\n");
+        if (sigcount==2) {
+                printf("\n\nGot a second signal - bailing out\n");
+                exit(1);
+        }
+}
 
 int init_resources() {
 	program = create_program("graph.v.glsl", "graph.f.glsl");
@@ -197,36 +337,7 @@ void display(int window_width,int window_height) {
 	// And we are done.
 
 	glDisableVertexAttribArray(attribute_coord2d);
-	//gluSwapBuffers();
 }
-/*
-void special(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_LEFT:
-		offset_x -= 0.03;
-		break;
-	case GLUT_KEY_RIGHT:
-		offset_x += 0.03;
-		break;
-	case GLUT_KEY_UP:
-		scale_x *= 1.5;
-		break;
-	case GLUT_KEY_DOWN:
-		scale_x /= 1.5;
-		break;
-	case GLUT_KEY_HOME:
-		offset_x = 0.0;
-		scale_x = 1.0;
-		break;
-	}
-
-	glutPostRedisplay();
-}
-
-void free_resources() {
-	glDeleteProgram(program);
-}
-*/
 
 void addpoint(float y){
 	float x = (x_pos - LEN/2) / (LEN/2.0);
@@ -246,11 +357,13 @@ void render(void) {
     display(window_width,window_height);
     glfwSwapBuffers(window);
     glfwPollEvents();
-
 }
 
-int main(int argc, char *argv[]) {
+int main(int ac, char** av) {
     struct itimerval it_val;	/* for setting itimer */
+    stlink_t* sl;
+    stlinky *st;
+    int add = 0;
     
     if (signal(SIGALRM, (void (*)(int)) render) == SIG_ERR) {
         perror("Unable to catch SIGALRM");
@@ -262,7 +375,7 @@ int main(int argc, char *argv[]) {
     it_val.it_interval = it_val.it_value;
         
     if (!glfwInit())
-            exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     window = glfwCreateWindow(640, 480, "Graph", NULL, NULL);
     if (!window)
     {
@@ -270,75 +383,105 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
-    //glfwSetKeyCallback(window, key_callback);
-    //glfwSetScrollCallback(window, scroll_callback);
-    //glfwSetMouseButtonCallback(window,mousebutton_callback);
-    //glfwSetCursorPosCallback(window,mousepos_callback);
 	
     if(!init_resources())
         exit(EXIT_FAILURE);        
     
 	for (int i = 0; i < LEN; i++) {
 		float x = (i - LEN/2) / (LEN/2.0);
-
 		graph[i].x = x;
-		graph[i].y = 0;//sin(x * 10.0) / (1.0 + x * x);
+		graph[i].y = 0;
 	}
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof graph, graph, GL_DYNAMIC_DRAW);
-    //glfwSwapInterval(0);
     
-    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
-        perror("error calling setitimer()");
-        exit(1);
-      }
-    
-    while (!glfwWindowShouldClose(window))
-    {
-        addpoint(0.1);
-        usleep(10000);
-        // Tell OpenGL to copy our array to the buffer object
-        /*
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    	glBufferData(GL_ARRAY_BUFFER, sizeof graph, graph, GL_DYNAMIC_DRAW);
-        glfwGetFramebufferSize(window, &window_width, &window_height);
-        display(window_width,window_height);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        */
-    }
 
+    
+    sl = stlink_open_usb(10, 1);
+            if (sl != NULL) {
+                    printf("ST-Linky proof-of-concept terminal :: Created by Necromant for lulz\n");
+                    stlink_version(sl);
+                    stlink_enter_swd_mode(sl);
+                    printf("chip id: %#x\n", sl->chip_id);
+                    printf("core_id: %#x\n", sl->core_id);
+
+                    cortex_m3_cpuid_t cpuid;
+                    stlink_cpu_id(sl, &cpuid);
+                    printf("cpuid:impl_id = %0#x, variant = %#x\n", cpuid.implementer_id, cpuid.variant);
+                    printf("cpuid:part = %#x, rev = %#x\n", cpuid.part, cpuid.revision);
+
+                    stlink_reset(sl);
+                    stlink_force_debug(sl);
+                    stlink_run(sl);
+                    stlink_status(sl);
+
+                    /* wait for device to boot */
+                    /* TODO: Make timeout adjustable via command line */
+                    sleep(1);
+
+                    if(ac == 1){ 
+                            st = stlinky_detect(sl);
+                    }else if(ac == 2){
+                            st = (stlinky*)malloc(sizeof(stlinky));
+                            st->sl = sl;
+                            st->off = (int)strtol(av[1], NULL, 16);
+                            printf("using stlinky at 0x%x\n", st->off);
+                            stlink_read_mem32(sl, st->off + 4, 4);
+                            st->bufsize = (size_t) *(unsigned char*) sl->q_buf;
+                            printf("stlinky buffer size 0x%zu \n", st->bufsize);
+                    }else{
+                            //goto bailout;
+                    }
+                    if (st == NULL)
+                    {
+                            printf("stlinky magic not found in sram :(\n");
+                            //goto bailout;
+                    }
+                    char* rxbuf = (char*)malloc(st->bufsize);
+                    char* txbuf = (char*)malloc(st->bufsize);
+                    size_t tmp;
+                    nonblock(1);
+                    int fd = fileno(stdin);
+                    int saved_flags = fcntl(fd, F_GETFL);
+                    fcntl(fd, F_SETFL, saved_flags & ~O_NONBLOCK);
+                    signal(SIGINT, cleanup);
+                    printf("Entering interactive terminal. CTRL+C to exit\n\n\n");
+                    /*if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
+                        perror("error calling setitimer()");
+                        exit(1);
+                    }*/
+                    add = 0;
+                    while(!glfwWindowShouldClose(window)) {
+                        if(add == 100){
+                            add = 0;
+                            render();
+                        }
+                        add++;
+                            if (stlinky_canrx(st)) {
+                                    tmp = stlinky_rx(st, rxbuf);
+
+                                    if(rxbuf[0] == 0x11)
+                                        addpoint(rxbuf[1]/128.0f);
+                                    else{
+                                        fwrite(rxbuf,tmp,1,stdout);
+                                        fflush(stdout);
+                                    }
+                            }
+                            if (kbhit()) {
+                                    tmp = read(fd, txbuf, st->bufsize);
+                                    stlinky_tx(st,txbuf,tmp);
+                            }
+                            if (!keep_running)
+                                    break;
+                    }
+            }
+    //bailout:
+    //            nonblock(0);
+    //    stlink_exit_debug_mode(sl);
+    //    stlink_close(sl);
+    //TODO: stop timer
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
-    
-    //glutInit(&argc, argv);
-	//glutInitDisplayMode(GLUT_RGB);
-	//glutInitWindowSize(640, 480);
-	//glutCreateWindow("My Graph");
-
-	//GLenum glew_status = glewInit();
-
-	//if (GLEW_OK != glew_status) {
-	//	fprintf(stderr, "Error: %s\n", glewGetErrorString(glew_status));
-	//	return 1;
-	//}
-
-	//if (!GLEW_VERSION_2_0) {
-	//	fprintf(stderr, "No support for OpenGL 2.0 found\n");
-	//	return 1;
-	//}
-
-	//printf("Use left/right to move horizontally.\n");
-	//printf("Use up/down to change the horizontal scale.\n");
-	//printf("Press home to reset the position and scale.\n");
-
-	//if (init_resources()) {
-	//	glutDisplayFunc(display);
-	//	glutSpecialFunc(special);
-	//	glutMainLoop();
-	//}
-
-	//free_resources();
 	return 0;
 }
