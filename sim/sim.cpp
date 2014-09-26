b50a17058b689cf586ac548776fe375c493deb24
crinq <crinq@crinqbook.fritz.box>
2014-09-26 19:28:38 +0200

    sim

--- sim/sim.cpp
+++ sim/sim.cpp
@@ -4,7 +4,7 @@
 int main(){
   float sim_time = 0;
   float sim_step = 0.001;
-  float sim_end_time = 20.0;
+  float sim_end_time = 500.0;
 
   srand(14235);
 
@@ -25,13 +25,14 @@ int main(){
   mot.elec_spec.v_rps = 0.852;
   mot.elec_spec.slip = 0;
 
-  mot.feedback.type = mot_c::feedback_s::ENC;
-  mot.feedback.count = 2048;
-  mot.noise.sin_scale = 0.8;
-  mot.noise.cos_scale = 0.8;
-  mot.noise.sin_offset = 0.0;
-  mot.noise.cos_offset = 0.0;
-  mot.noise.var = 0.01;
+  mot.feedback.type = mot_c::feedback_s::RES;
+  mot.feedback.count = 1;
+  mot.feedback.res_offset = 0.0;
+  mot.noise.sin_scale = 1.0;
+  mot.noise.cos_scale = 1.0;
+  mot.noise.sin_offset = 0.1;
+  mot.noise.cos_offset = -0.2;
+  mot.noise.var = 0.05;
 
   mot.load.friction = 0.0;
   mot.load.load = 0.0;
@@ -41,9 +42,9 @@ int main(){
 
   cmd_c cmd;
   cmd.reset();
-  cmd.periode = 10.0;
-  cmd.amplitude = 1.0;
-  cmd.wave = cmd_c::SINE;
+  cmd.periode = 50.0;
+  cmd.amplitude = 0.5;
+  cmd.wave = cmd_c::RAMP;
   cmd.type = cmd_c::POS;
   cmd.pos_res = 0.01;
   cmd.vel_res = 0.01;
@@ -51,19 +52,23 @@ int main(){
 
   drive_c drive;
   drive.reset();
-  drive.dc = 50;
+  drive.dc = 30;
   drive.pwm_scale = 0.9;
   drive.pwm_res = 8400;
   drive.mot = &mot;
   drive.in = &cmd;
+  drive.input = input;
+  drive.pid = pid;
+  drive.output = output;
 
-  cout << flush << "time, cmd, pos, error, load" << endl;
+
+  cout << flush << "time cmd pos est_pos sin_avg sin_scale cos_avg cos_scale" << endl;
 
   for(sim_time = 0.0; sim_time < sim_end_time; sim_time += sim_step){
     drive.in->step(sim_step);
     drive.step(sim_step);
     drive.mot->step(sim_step);
-    cout << sim_time << ", " << drive.in->get_pos() << ", " << drive.mot->state.pos << ", " << minus_(drive.cmd.pos, drive.est.pos) << ", " << drive.state.ctr << endl;
+    cout << sim_time << ", " << drive.in->get_pos() << ", " << drive.mot->state.pos << ", " << drive.est.pos << ", " << drive.est.sin_avg << ", " << drive.est.sin_scale << ", " << drive.est.cos_avg << ", " << drive.est.cos_scale << endl;
   }
 
   system("gnuplot --persist gp");
