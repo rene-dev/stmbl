355c0b74e9f888a77b9c0398889df6f884646e1f
Rene Hopf <renehopf@mac.com>
2014-11-25 01:19:39 +0100

    License

--- src/pid.c
+++ src/pid.c
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the stmbl project.
+ *
+ * Copyright (C) 2013-2015 Rene Hopf <renehopf@mac.com>
+ * Copyright (C) 2013-2015 Nico Stute <crinq@crinq.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "pid.h"
 
 float minus(float a, float b){
