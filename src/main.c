3c164f181da2bb899dac3586ad896d3fb3bb4502
Rene Hopf <renehopf@mac.com>
2014-12-18 14:26:37 +0100

    moved usart init to setup.c

--- src/main.c
+++ src/main.c
@@ -234,38 +234,6 @@ void TIM5_IRQHandler(void){ //5KHz
 	}
 }
 
-void usart_init(){
-	GPIO_InitTypeDef GPIO_InitStruct;
-	USART_InitTypeDef USART_InitStruct;
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	//USART TX
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP ;
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-	//USART RX
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-	USART_InitStruct.USART_BaudRate = 2000000;//2000000
-	USART_InitStruct.USART_WordLength = USART_WordLength_9b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART3, &USART_InitStruct);
-	/* Enable the USART */
-	USART_Cmd(USART3, ENABLE);
-}
-
 #define DATALENGTH 3
 
 typedef union{
@@ -341,7 +309,6 @@ int main(void)
 	link_hal_pins("pwm2uart0.vout", "net0.v");
 	link_hal_pins("pwm2uart0.wout", "net0.w");
 
-	usart_init();
 	data_t data;
 
 	while(1)  // Do not exit
--- src/setup.c
+++ src/setup.c
@@ -54,6 +54,7 @@ void setup(){
     setup_adc();
     //setup_dma();
     setup_pid_timer();
+	setup_usart();
 
 	// systick timer
 	time = 0;
@@ -67,6 +68,38 @@ void setup(){
     #ifdef USBTERM
     UB_USB_CDC_Init();
     #endif
+}
+
+void setup_usart(){
+	GPIO_InitTypeDef GPIO_InitStruct;
+	USART_InitTypeDef USART_InitStruct;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	//USART TX
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP ;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+	//USART RX
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+	USART_InitStruct.USART_BaudRate = 2000000;//2000000
+	USART_InitStruct.USART_WordLength = USART_WordLength_9b;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART3, &USART_InitStruct);
+	/* Enable the USART */
+	USART_Cmd(USART3, ENABLE);
 }
 
 
