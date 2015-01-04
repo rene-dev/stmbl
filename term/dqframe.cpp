d6854a8f18482cd418827a8228d248e3448d6dde
Rene Hopf <renehopf@mac.com>
2015-01-04 02:44:46 +0100

    dq

--- term/dqframe.cpp
+++ term/dqframe.cpp
@@ -6,7 +6,11 @@ using std::string;
 using std::to_string;
 
 dqFrame::dqFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title){
+    wxBoxSizer *mainsizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *datasizer = new wxBoxSizer(wxHORIZONTAL);
     dqdraw = new dqDraw(this);
+    mainsizer->Add(dqdraw, 1,wxEXPAND,0);
+    mainsizer->Add(datasizer, 1,wxEXPAND,0);
     
     //topsizer->Add(drawpanel, 1,wxEXPAND,0);
 }
