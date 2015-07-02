27dab0e78dcd3d0e6c246e022adc50598d66f655
Rene Hopf <renehopf@mac.com>
2015-07-02 14:49:10 +0200

    f1 struct

--- stm32f103/src/main.c
+++ stm32f103/src/main.c
@@ -1,53 +1,53 @@
 #include "stm32f10x_conf.h"
 #include <math.h>
 
-/*
-struct f1tof4{
-  int16 ia;
-  int16 ib;
-  int16 ua;
-  int16 ub;
-  int16 udc;
-  int16 value;
-  struct {
-    bool enabled;
-    bool temp_limit;
-    bool current_limit;
-    bool watchdog; // toggle
-    int4 misc;
-  }flags;
-  int8 crc;
-};
 
-struct f4tof1{
-  int16 a; // u/i
-  int16 b; // u/i
-  int16 value;
-  int6 address; 
-  /* 
-    rw:
-      r
-      l
-      max_i
-      max_temp
-      max_pwm
-      phase_offset_uv
-      phase_offset_vw
-      svm_mode (centered, svm, bottom low)
-      u/i mode
-    
-    r:
-      temp0
-      temp1
-      temp2
-      temp3
-      hw version
-  */  
-  bool enable;
-  bool watchdog; // toggle in hal
-  int8 crc;
-};
-*/
+// struct f1tof4{
+//   int16 ia;
+//   int16 ib;
+//   int16 ua;
+//   int16 ub;
+//   int16 udc;
+//   int16 value;
+//   struct {
+//     bool enabled;
+//     bool temp_limit;
+//     bool current_limit;
+//     bool watchdog; // toggle
+//     int4 misc;
+//   }flags;
+//   int8 crc;
+// };
+//
+// struct f4tof1{
+//   int16 a; // u/i
+//   int16 b; // u/i
+//   int16 value;
+//   int6 address;
+//   /*
+//     rw:
+//       r
+//       l
+//       max_i
+//       max_temp
+//       max_pwm
+//       phase_offset_uv
+//       phase_offset_vw
+//       svm_mode (centered, svm, bottom low)
+//       u/i mode
+//
+//     r:
+//       temp0
+//       temp1
+//       temp2
+//       temp3
+//       hw version
+//   */
+//   bool enable;
+//   bool watchdog; // toggle in hal
+//   int8 crc;
+// };
+
 
 #ifndef M_PI
 #define M_PI		3.14159265358979323846
