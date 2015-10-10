5d498a9dcb8cd661d791f12b9103462f67b99ddd
crinq <crinq@crinq.de>
2015-10-11 00:00:26 +0200

    cobs

---
+++ stm32f103/src/common.c
@@ -0,0 +1,26 @@
+#include "common.h"
+
+void buff_packet(packet_header_t* p, uint8_t size){
+   uint8_t nonkey_count = 0;
+   uint8_t* buf = ((uint8_t*)p) + sizeof(packet_header_t);
+   for(int i = size - 1; i >= 0; i--){
+      if(buf[i] == p->start){
+         buf[i] = nonkey_count;
+         nonkey_count = 0;
+      }
+      else{
+         nonkey_count++;
+      }
+   }
+   p->key = nonkey_count;
+}
+
+void unbuff_packet(packet_header_t* p, uint8_t size){
+   uint8_t temp;
+   uint8_t* buf = ((uint8_t*)p) + sizeof(packet_header_t);
+   for(int j = p->key; j < size;){
+      temp = buf[j];
+      buf[j] = p->start;
+      j += temp + 1;
+   }
+}
