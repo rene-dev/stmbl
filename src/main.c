e6120d4c660be2062df0740adbf3c9736a6cccb0
Rene Hopf <renehopf@mac.com>
2015-11-04 19:39:34 +0100

    cmd rx lockout

--- src/main.c
+++ src/main.c
@@ -79,8 +79,8 @@ int main(void)
    #include "comps/encs.comp"
 
    #include "comps/fault.comp"
-   #include "comps/enc_cmd.comp"
-   //#include "comps/enc_fb.comp"
+   //#include "comps/enc_cmd.comp"
+   #include "comps/enc_fb.comp"
    //#include "comps/enc_fb_org.comp"
 
    //#include "comps/en.comp"
