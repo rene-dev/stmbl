1ed3774f4e674646c839d529fc8c7778a0acb6fd
Nicolas Reinecke <nr@das-labor.org>
2016-01-04 14:57:40 +0100

    HV: simplify clock config
    The default clock config of system_stm32f10x.c is HSI 8 MHz.
    Configure the clock to 72 MHz with PLL and 8MHz HSE.
    Remove clock reinit with the same settings.

--- stm32f103/src/main.c
+++ stm32f103/src/main.c
@@ -100,24 +100,6 @@ void SysTick_Handler(void)
 
 void RCC_Configuration(void)
 {
-	ErrorStatus HSEStartUpStatus;
-	RCC_DeInit();
-	RCC_HSEConfig(RCC_HSE_ON);
-	HSEStartUpStatus = RCC_WaitForHSEStartUp();
-
-	if(HSEStartUpStatus == SUCCESS){
-		FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-		FLASH_SetLatency(FLASH_Latency_2);
-		RCC_HCLKConfig(RCC_SYSCLK_Div1);
-		RCC_PCLK2Config(RCC_HCLK_Div1);
-		RCC_PCLK1Config(RCC_HCLK_Div2);
-		RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
-		RCC_PLLCmd(ENABLE);
-		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-		while(RCC_GetSYSCLKSource() != 0x08){}
-	}
-
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
 }
 
