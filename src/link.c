02f5e3b735b6ac09a9c097e15d960aa09697bad4
Rene Hopf <renehopf@mac.com>
2016-06-17 22:34:14 +0200

    fixed fault

--- src/link.c
+++ src/link.c
@@ -291,7 +291,7 @@ int update_cmd(){
          hal_set_pin("enc_cmd0.rt_prio", 2.0);
          if(hal_get_pin("conf0.error_out") == 1.0){//error out using rs485
             hal_set_pin("en0.rt_prio", 15.0);
-            hal_set_pin("en0.en", 1.0);//TODO: link to error pin
+            hal_link_pins("fault0.fault_not", "en0.en");
             hal_set_pin("en0.txen", 1.0);
          }
          break;
