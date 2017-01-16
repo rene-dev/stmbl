bbd8b8fdb5fae5a013b760860a885e3db75842f0
Rene Hopf <renehopf@mac.com>
2017-01-16 17:16:53 +0100

    lowside communitation comp

--- stm32f303/src/usart.c
+++ stm32f303/src/usart.c
@@ -52,7 +52,6 @@
 /* USER CODE END 0 */
 
 UART_HandleTypeDef huart1;
-UART_HandleTypeDef huart3;
 
 /* USART1 init function */
 
@@ -80,20 +79,7 @@ void MX_USART1_UART_Init(void)
 void MX_USART3_UART_Init(void)
 {
 
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 38400;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
+
 
 }
 
