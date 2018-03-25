9e6a9d6645cdba3888450a809be961c4d2bd9550
Rene Hopf <renehopf@mac.com>
2018-03-25 03:11:12 +0200

    fanuc encoder request via spi, led, error

--- src/comps/encf.c
+++ src/comps/encf.c
@@ -18,7 +18,6 @@ HAL_PIN(com_pos);
 HAL_PIN(index);
 
 volatile uint16_t tim_data[160];
-uint32_t count;
 
 #pragma pack(1)
 typedef struct{
@@ -48,10 +47,6 @@ static void nrt_init(volatile void *ctx_ptr, volatile hal_pin_inst_t *pin_ptr) {
   // struct encf_ctx_t *ctx = (struct encf_ctx_t *)ctx_ptr;
   // struct encf_pin_ctx_t * pins = (struct encf_pin_ctx_t *)pin_ptr;
   GPIO_InitTypeDef GPIO_InitStruct;
-  USART_InitTypeDef USART_InitStruct;
-  DMA_InitTypeDef DMA_InitStructurerx;
-
-  RCC_APB1PeriphClockCmd(FB0_ENC_TIM_RCC, ENABLE);
 
   //TX enable
   GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_15;
@@ -60,17 +55,6 @@ static void nrt_init(volatile void *ctx_ptr, volatile hal_pin_inst_t *pin_ptr) {
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
-
-  //USART TX
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
-  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_6;
-  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_UP;
-  GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   //TIM CH1 A
   GPIO_InitStruct.GPIO_Pin   = FB0_A_PIN;
@@ -81,44 +65,58 @@ static void nrt_init(volatile void *ctx_ptr, volatile hal_pin_inst_t *pin_ptr) {
   GPIO_Init(FB0_A_PORT, &GPIO_InitStruct);
   GPIO_PinAFConfig(FB0_A_PORT, FB0_A_PIN_SOURCE, FB0_ENC_TIM_AF);
 
-  USART_InitStruct.USART_BaudRate            = 128000;
-  USART_InitStruct.USART_WordLength          = USART_WordLength_8b;
-  USART_InitStruct.USART_StopBits            = USART_StopBits_1;
-  USART_InitStruct.USART_Parity              = USART_Parity_No;
-  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStruct.USART_Mode                = USART_Mode_Tx;
-  USART_Init(USART6, &USART_InitStruct);
-
-  /* Enable the USART */
-  USART_Cmd(USART6, ENABLE);
-
   //TIM rx dma
   //fb0 rx auf 12: tim4_ch1 dma1_0_2
   //fb1 rx auf 12: tim1_ch1 dma2_3_6
-  DMA_InitStructurerx.DMA_Channel            = DMA_Channel_2;
-  DMA_InitStructurerx.DMA_PeripheralBaseAddr = (uint32_t)&FB0_ENC_TIM->CCR1;
-  DMA_InitStructurerx.DMA_Memory0BaseAddr    = (uint32_t)&tim_data;
-  DMA_InitStructurerx.DMA_DIR                = DMA_DIR_PeripheralToMemory;
-  DMA_InitStructurerx.DMA_BufferSize         = ARRAY_SIZE(tim_data);
-  DMA_InitStructurerx.DMA_PeripheralInc      = DMA_PeripheralInc_Disable;
-  DMA_InitStructurerx.DMA_MemoryInc          = DMA_MemoryInc_Enable;
-  DMA_InitStructurerx.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-  DMA_InitStructurerx.DMA_MemoryDataSize     = DMA_PeripheralDataSize_HalfWord;
-  DMA_InitStructurerx.DMA_Mode               = DMA_Mode_Normal;
-  DMA_InitStructurerx.DMA_Priority           = DMA_Priority_VeryHigh;
-  DMA_InitStructurerx.DMA_FIFOMode           = DMA_FIFOMode_Disable;
-  DMA_InitStructurerx.DMA_FIFOThreshold      = DMA_FIFOThreshold_HalfFull;
-  DMA_InitStructurerx.DMA_MemoryBurst        = DMA_MemoryBurst_Single;
-  DMA_InitStructurerx.DMA_PeripheralBurst    = DMA_PeripheralBurst_Single;
+  DMA_InitTypeDef DMA_InitStruct;
+  DMA_InitStruct.DMA_Channel            = DMA_Channel_2;
+  DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&FB0_ENC_TIM->CCR1;
+  DMA_InitStruct.DMA_Memory0BaseAddr    = (uint32_t)&tim_data;
+  DMA_InitStruct.DMA_DIR                = DMA_DIR_PeripheralToMemory;
+  DMA_InitStruct.DMA_BufferSize         = ARRAY_SIZE(tim_data);
+  DMA_InitStruct.DMA_PeripheralInc      = DMA_PeripheralInc_Disable;
+  DMA_InitStruct.DMA_MemoryInc          = DMA_MemoryInc_Enable;
+  DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStruct.DMA_MemoryDataSize     = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStruct.DMA_Mode               = DMA_Mode_Normal;
+  DMA_InitStruct.DMA_Priority           = DMA_Priority_VeryHigh;
+  DMA_InitStruct.DMA_FIFOMode           = DMA_FIFOMode_Disable;
+  DMA_InitStruct.DMA_FIFOThreshold      = DMA_FIFOThreshold_HalfFull;
+  DMA_InitStruct.DMA_MemoryBurst        = DMA_MemoryBurst_Single;
+  DMA_InitStruct.DMA_PeripheralBurst    = DMA_PeripheralBurst_Single;
   DMA_Cmd(DMA1_Stream0, DISABLE);
   DMA_DeInit(DMA1_Stream0);
-  DMA_Init(DMA1_Stream0, &DMA_InitStructurerx);
+  DMA_Init(DMA1_Stream0, &DMA_InitStruct);
+
   //timer setup
+  RCC_APB1PeriphClockCmd(FB0_ENC_TIM_RCC, ENABLE);
   FB0_ENC_TIM->CR1 &= ~TIM_CR1_CEN; 
   FB0_ENC_TIM->CCMR1 = TIM_CCMR1_CC1S_0; // cc1 input ti1
   FB0_ENC_TIM->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P | TIM_CCER_CC1NP; // cc1 en, rising edge, falling edge
   FB0_ENC_TIM->ARR = 65535;
   FB0_ENC_TIM->DIER = TIM_DIER_CC1DE; // enable cc1 dma reeuest
+
+  //SPI is used to generate request
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+  SPI_InitTypeDef SPI_InitTypeDefStruct;
+  SPI_InitTypeDefStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+  SPI_InitTypeDefStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+  SPI_InitTypeDefStruct.SPI_Mode = SPI_Mode_Master;
+  SPI_InitTypeDefStruct.SPI_DataSize = SPI_DataSize_8b;
+  SPI_InitTypeDefStruct.SPI_NSS = SPI_NSS_Soft;
+  SPI_InitTypeDefStruct.SPI_FirstBit = SPI_FirstBit_MSB;
+  SPI_InitTypeDefStruct.SPI_CPOL = SPI_CPOL_High;
+  SPI_InitTypeDefStruct.SPI_CPHA = SPI_CPHA_2Edge;
+  SPI_Init(SPI3, &SPI_InitTypeDefStruct);
+  
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_SPI3);
+  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_12;
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+  SPI_Cmd(SPI3, ENABLE);
 
   GPIO_SetBits(GPIOD, GPIO_Pin_15);  //tx enable
 }
@@ -127,7 +125,7 @@ static void rt_func(float period, volatile void *ctx_ptr, volatile hal_pin_inst_
   // struct encf_ctx_t *ctx      = (struct encf_ctx_t *)ctx_ptr;
   struct encf_pin_ctx_t *pins = (struct encf_pin_ctx_t *)pin_ptr;
 
-  count = ARRAY_SIZE(tim_data) - DMA1_Stream0->NDTR;
+  uint32_t count = ARRAY_SIZE(tim_data) - DMA1_Stream0->NDTR;
   PIN(dma) = count;
 
   for(int i = 0; i < 10; i++){
@@ -135,34 +133,38 @@ static void rt_func(float period, volatile void *ctx_ptr, volatile hal_pin_inst_
   }
   
   uint8_t bits_sum = 0;
-  for(int i = 1; i < count; i++){
+  for(int i = 1; i < count; i++){//each capture form dma
+    //1 bit = 82 ticks (1.024e-6)/(1/82e6)
+    //calculate time between edges
     uint16_t diff = tim_data[i] - tim_data[i-1];
-    //1 bit = 82 ticks
-    //(1.024e-6)/(1/82e6)
+    //number of bits to set
     int bits = (float)diff/(float)81;
-    if(i%2 == 0){
+    if(i%2 == 0){//line starts high, set every even numbered captures to 1
       for(int j = bits_sum; j < bits + bits_sum; j++){
         data.enc_data[j/8] |= (1 << j%8);
       }
     }
     bits_sum += bits;
   }
-
-  uint32_t pos;
-  pos = data.fanuc.pos_lo + (data.fanuc.pos_hi << 6);
-  PIN(pos) = ((float)pos * 2.0 * M_PI / (1<<22)) - M_PI;
-  PIN(turns) = data.fanuc.turns;
-  pos = data.fanuc.com_pos;
-  PIN(com_pos) = (pos * 2.0 * M_PI / 1024) - M_PI;
-  PIN(index) = data.fanuc.no_index;
-
+  if(bits_sum > 50){
+    uint32_t pos;
+    pos = data.fanuc.pos_lo + (data.fanuc.pos_hi << 6);
+    PIN(pos) = ((float)pos * 2.0 * M_PI / (1<<22)) - M_PI;
+    PIN(turns) = data.fanuc.turns;
+    pos = data.fanuc.com_pos;
+    PIN(com_pos) = (pos * 2.0 * M_PI / 1024) - M_PI;
+    PIN(index) = data.fanuc.no_index;
+    PIN(error) = 0;
+  }else{
+    PIN(error) = 1;
+  }
   //reset timer
   FB0_ENC_TIM->CNT = 0;
   FB0_ENC_TIM->CCR1 = 0;
   FB0_ENC_TIM->CR1 |= TIM_CR1_CEN; // enable tim
 
   //send request
-  USART6->DR = 0xff;
+  SPI3->DR = 0x7c;
   //start DMA
   DMA_Cmd(DMA1_Stream0, DISABLE);
   DMA_ClearFlag(DMA1_Stream0, DMA_FLAG_TCIF0);
