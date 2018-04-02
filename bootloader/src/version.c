bde86bb923e0ae295ef8e966b5a8920472f70ac9
Rene Hopf <renehopf@mac.com>
2018-04-03 00:32:56 +0200

    update version

--- bootloader/src/version.c
+++ bootloader/src/version.c
@@ -4,4 +4,4 @@ volatile const struct version_info version_info = {
     .product_name = "STMBL-Bootloader",
     .major        = 0,
     .minor        = 9,
-    .patch        = 0};
+    .patch        = 14};
--- stm32f303/src/version.c
+++ stm32f303/src/version.c
@@ -4,4 +4,4 @@ volatile const struct version_info version_info = {
     .product_name = "STMBL-hv-f303",
     .major        = 0,
     .minor        = 9,
-    .patch        = 0};
+    .patch        = 14};
