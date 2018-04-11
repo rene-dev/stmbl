4a02ac376d0307d534d85cca365ceee9484325dc
Rene Hopf <renehopf@mac.com>
2018-04-12 01:31:15 +0200

    global version number

--- bootloader/src/version.c
+++ bootloader/src/version.c
@@ -1,7 +1,8 @@
 #include "version.h"
+#include "version_number.h"
 
 volatile const version_info_t version_info __attribute__((section(".version_info"))) = {
     .product_name = "STMBL-Bootloader",
-    .major        = 0,
-    .minor        = 9,
-    .patch        = 15};
+    .major        = VERSION_MAJOR,
+    .minor        = VERSION_MINOR,
+    .patch        = VERSION_PATCH};
