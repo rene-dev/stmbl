b4a6b16c4c50155e3e4f17f4364487f5a6ec611d
Nico Stute <crinq@users.noreply.github.com>
2018-10-13 16:48:42 +0200

    Update stmbl_talk.h

--- shared/stmbl_talk.h
+++ shared/stmbl_talk.h
@@ -4,7 +4,7 @@
 #pragma pack(push, 1)
 typedef struct {
   uint32_t crc;        // can be 0 for eth comm
-  uint8_t slave_addr;  // calc from uid, overwrite in config
+  uint8_t slave_addr;  // calc from uid, overwrite in config, bootloader = 255
   uint8_t len;         // #data in words
   uint8_t conf_addr;
   struct {
@@ -13,9 +13,7 @@ typedef struct {
       WRITE_CONF,  // conf[rx.header.conf_addr] = rx.header.config.u32, tx.header.config.u32 = conf[tx.header.conf_addr++]
       READ_CONF,   // tx.header.config.u32 = conf[rx.header.conf_addr]
       DO_RESET,
-      BOOTLOADER,
-      // ENABLE,
-      // DISABLE, // proc / conf data = default
+      BOOTLOADER
     } cmd : 4;
     uint8_t counter : 4;  // packet counter, master: ++, slave: same as request
   } flags;
