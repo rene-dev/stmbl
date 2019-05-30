78586d740ed1481684367704136de68cdf02d1ec
crinq <crinq@crinq.de>
2019-05-30 11:08:39 +0200

    sensorless

--- shared/comps/field.c
+++ shared/comps/field.c
@@ -28,7 +28,8 @@
 HAL_COMP(field);
 
 //in
-HAL_PIN(vel_n);
+HAL_PIN(vele_n);
+HAL_PIN(velm_n);
 HAL_PIN(i_n);
 HAL_PIN(t_n);
 HAL_PIN(vel_boost);
@@ -36,18 +37,31 @@ HAL_PIN(t_boost);
 
 HAL_PIN(vel);
 
+HAL_PIN(vel_mode);
+HAL_PIN(in_mode);
+
+// inout
+HAL_PIN(t);
+HAL_PIN(iq);
+
 //out
 HAL_PIN(id);
+
 HAL_PIN(iq_max);
 HAL_PIN(iq_min);
 
 HAL_PIN(t_max);
 HAL_PIN(t_min);
 
+HAL_PIN(slip);
+HAL_PIN(velm);
+HAL_PIN(vele);
+
 static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   // struct field_ctx_t *ctx      = (struct field_ctx_t *)ctx_ptr;
   struct field_pin_ctx_t *pins = (struct field_pin_ctx_t *)pin_ptr;
-  PIN(vel_n) = 50.0 * 2.0 * M_PI / 2.0;
+  PIN(vele_n) = 60.0 * 2.0 * M_PI / 2.0;
+  PIN(velm_n) = 1745.0 / 60.0 * 2.0 * M_PI;
   PIN(i_n) = 15.0;
   PIN(t_n) = 23.0;
   PIN(vel_boost) = 1.0;
@@ -58,40 +72,69 @@ static void rt_func(float period, void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   // struct field_ctx_t *ctx      = (struct field_ctx_t *)ctx_ptr;
   struct field_pin_ctx_t *pins = (struct field_pin_ctx_t *)pin_ptr;
 
-  float vel = MAX(ABS(PIN(vel)), 0.01);
+  float vel = MAX(ABS(PIN(vele)), 0.01);
   float i_n = PIN(i_n);
   float t_n = MAX(PIN(t_n), 0.01);
-  float vel_n = PIN(vel_n);
+  float vele_n = PIN(vele_n);
+  float velm_n = PIN(velm_n);
   float t_boost = PIN(t_boost);
   float vel_boost = PIN(vel_boost);
+  float slip_n = vele_n - velm_n;
+  float t = 0;
+  float iq = 0;
+  float slip = 0;
   
+
   // float p_max = vel_n * t_n * vel_boost;
   // float t_max = MIN(t_n * t_boost, p_max / vel);
 
   float id_n = i_n / sqrtf(2.0);
-  float id = MIN(id_n, id_n * vel_n * vel_boost / vel);
+  float id = MIN(id_n, id_n * vele_n * vel_boost / vel);
 
   float iq_max = 0;
   float iq_min = 0;
   
   if(PIN(vel) > 0.0){
-    iq_max = MIN(id_n * t_boost, id_n * vel_n * vel_boost / vel);
+    iq_max = MIN(id_n * t_boost, id_n * vele_n * vel_boost / vel);
     iq_min = -id_n * t_boost;
   }
   else{
     iq_max = id_n * t_boost;
-    iq_min = -MIN(id_n * t_boost, id_n * vel_n * vel_boost / vel);
+    iq_min = -MIN(id_n * t_boost, id_n * vele_n * vel_boost / vel);
   }
 
   float t_max = iq_max * id / id_n / id_n * t_n;
   float t_min = iq_min * id / id_n / id_n * t_n;
   
+  if(PIN(in_mode) > 0){ // ipnut = torque
+    t = PIN(t);
+    t = CLAMP(t, t_min, t_max);
+    iq = t / t_n / id * id_n * id_n;
+    PIN(iq) = iq;
+  }
+  else{ // input = iq
+    iq = PIN(iq);
+    iq = CLAMP(iq, iq_min, iq_max);
+    t = iq * id / id_n / id_n * t_n;
+    PIN(t) = t;
+  }
+  slip = slip_n / t_n * t;
+
+  if(PIN(vel_mode) > 0){ // input = velm
+    PIN(vele) = PIN(vel) + slip;
+    PIN(velm) = PIN(vel);
+  }
+  else{ // input = vele
+    PIN(vele) = PIN(vel);
+    PIN(velm) = PIN(vel) - slip;
+  }
+
+  PIN(slip) = slip;
   PIN(id) = id;
   PIN(iq_max) = iq_max;
   PIN(iq_min) = iq_min;
   PIN(t_max) = t_max;
-  PIN(t_min) = t_min;
-   
+  PIN(t_min) = t_min;  
 }
 
 hal_comp_t field_comp_struct = {
