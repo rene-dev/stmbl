043a59669f6896c6c5765d5e97ca7777b9114791
crinq <crinq@crinq.de>
2019-06-03 23:20:33 +0200

    acim foo

--- shared/comps/acim_ttc.c
+++ shared/comps/acim_ttc.c
@@ -6,68 +6,103 @@
 
 HAL_COMP(acim_ttc);
 
-HAL_PIN(mode);  // 0 = u/f, 1 = mtpa
+HAL_PIN(mode);  // 0 = slip, 1 = mtpa, 2 = u/f
+HAL_PIN(sensorless);
 
 // motor values
 HAL_PIN(torque_n);
 HAL_PIN(cur_n);
-HAL_PIN(slip_n);
+// HAL_PIN(slip_n);
 HAL_PIN(polecount);
 HAL_PIN(freq_n);
+HAL_PIN(vel_n);
 HAL_PIN(u_n);
 HAL_PIN(u_boost);
+HAL_PIN(t_boost);
+HAL_PIN(v_boost);
+HAL_PIN(s_boost);
 
 // torque cmd in
 HAL_PIN(torque);
-HAL_PIN(vel);
+HAL_PIN(vel_m);
 
 // cur cmd out
 HAL_PIN(d_cmd);
 HAL_PIN(q_cmd);
 HAL_PIN(cmd_mode);
-HAL_PIN(freq);
-HAL_PIN(slip)
 HAL_PIN(pos);
+HAL_PIN(vel_e);
+HAL_PIN(slip);
+
+HAL_PIN(t_min);
+HAL_PIN(t_max);
+
+static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
+  struct acim_ttc_pin_ctx_t *pins = (struct acim_ttc_pin_ctx_t *)pin_ptr;
+  PIN(polecount) = 2;
+  PIN(vel_n) = 1745.0 / 60.0 * 2.0 * M_PI;
+  PIN(torque_n) = 23.0;
+  PIN(cur_n) = 17.0;
+  PIN(freq_n) = 60.0;
+  PIN(u_n) = 80.0;
+  PIN(u_boost) = 7.0;
+  PIN(t_boost) = 1.3;
+  PIN(v_boost) = 1.15;
+  PIN(s_boost) = 2.5;
+  PIN(mode) = 0;
+  PIN(sensorless) = 0;
+}
 
 static void rt_func(float period, void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   // struct acim_ttc_ctx_t * ctx = (struct acim_ttc_ctx_t *)ctx_ptr;
   struct acim_ttc_pin_ctx_t *pins = (struct acim_ttc_pin_ctx_t *)pin_ptr;
 
   float poles    = MAX(PIN(polecount), 1.0);
-  float torque   = PIN(torque);
-  float vel      = PIN(vel);
-  float slip_n   = PIN(slip_n);
-  float torque_n = MAX(PIN(torque_n), 0.001);
-  float cur_n    = PIN(cur_n);
+  float vel_n    = PIN(vel_n) * poles;
+  float t_n      = MAX(PIN(torque_n), 0.001);
   float freq_n   = MAX(PIN(freq_n), 1.0);
+  float slip_n   = vel_n - freq_n * 2.0 * M_PI;
+  float cur_n    = PIN(cur_n);
   float u_n      = PIN(u_n);
   float u_boost  = PIN(u_boost);
+  float t_boost  = PIN(t_boost);
+  float v_boost  = PIN(v_boost);
 
-  float d_cmd   = 0.0;
-  float q_cmd   = 0.0;
-  float freq = vel * poles / 2.0 / M_PI;
-  float slip = 0.0;
+  float torque   = PIN(torque);
+  float vel      = 0.0;
+  if(PIN(sensorless) > 0.0){
+    vel = PIN(vel_e);
+  }
+  else{
+    vel = PIN(vel_m) * poles;
+  }
+
+  float d_cmd    = 0.0;
+  float q_cmd    = 0.0;
+  float slip     = 0.0;
   float cmd_mode = 0;
+
+  float id_n = cur_n / sqrtf(2.0);
 
   switch((int)PIN(mode)) {
     case 0: // slip control
       cmd_mode = 1.0; // cur cmd
-      d_cmd   = cur_n / sqrtf(2.0);  // constant flux
-      q_cmd   = cur_n / sqrtf(2.0) / torque_n * torque;
-      slip = slip_n / torque_n * torque;
+      d_cmd = MIN(id_n, id_n * freq_n * 2.0 * M_PI * v_boost / vel); // constant flux
+      q_cmd   = id_n / t_n * torque;
+      slip = slip_n / t_n * torque;
       break;
 
     case 1: // mtpa
       cmd_mode = 1.0; // cur cmd
       d_cmd   = 0.0;
-      q_cmd   = cur_n / torque_n * torque;
+      q_cmd   = cur_n / t_n * torque;
       slip = slip_n * SIGN(torque);  // constant slip
       break;
 
-    case 2: // u/f
+    case 2: // u/f slip
       cmd_mode = 0; // volt cmd
-      slip = slip_n / torque_n * torque;
-      d_cmd = MAX(u_n / freq_n * ABS(freq), u_boost);
+      slip = slip_n / t_n * torque;
+      d_cmd = MAX(u_n / freq_n * ABS(vel / 2.0 / M_PI), u_boost);
       q_cmd = 0.0;
       break;
 
@@ -78,15 +113,38 @@ static void rt_func(float period, void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
       slip = 0.0;
   }
 
-  slip = LIMIT(slip, slip_n * 4.0);
-  freq += slip;
+  float t_min = 0;
+  float t_max = 0;
+
+  if(PIN(vel_m) > 0.0){
+    t_max = t_n * MIN(t_boost, vel_n * v_boost / vel);
+    t_min = -t_max * t_boost;
+  }
+  else{
+    t_min = -t_n * MIN(t_boost, vel_n * v_boost / vel);
+    t_max = -t_min * t_boost;
+  }
+
+  slip = LIMIT(slip, slip_n * PIN(s_boost));
+
+  if(PIN(sensorless) > 0.0){
+    vel -= slip;
+    PIN(vel_m) = vel * poles;
+  }
+  else{
+    vel += slip;
+    PIN(vel_e) = vel;
+  }
+
+  PIN(t_min) = t_min;
+  PIN(t_max) = t_max;
 
   PIN(cmd_mode) = cmd_mode;
   PIN(d_cmd)   = d_cmd;
   PIN(q_cmd)   = q_cmd;
-  PIN(freq) = freq;
+
   PIN(slip) = slip;
-  PIN(pos)  = mod(PIN(pos) + freq * period * 2.0 * M_PI);
+  PIN(pos)  = mod(PIN(pos) + vel * period);
 }
 
 hal_comp_t acim_ttc_comp_struct = {
@@ -94,7 +152,7 @@ hal_comp_t acim_ttc_comp_struct = {
     .nrt       = 0,
     .rt        = rt_func,
     .frt       = 0,
-    .nrt_init  = 0,
+    .nrt_init  = nrt_init,
     .rt_start  = 0,
     .frt_start = 0,
     .rt_stop   = 0,
