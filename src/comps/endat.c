e9a0898f487565b75914acc19a919879f0de5579
Rene Hopf <renehopf@mac.com>
2019-06-29 02:07:27 +0200

    endat wip

--- src/comps/endat.c
+++ src/comps/endat.c
@@ -9,6 +9,18 @@
 
 HAL_COMP(endat);
 
+HAL_PIN(req);
+HAL_PIN(pos_len);
+HAL_PIN(mpos_len);
+
+HAL_PIN(pos);
+HAL_PIN(mpos);
+
+HAL_PIN(f1);
+HAL_PIN(f2);
+HAL_PIN(shift);
+
+
 HAL_PINA(byte, 6);
 
 static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
@@ -35,7 +47,7 @@ static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   //SPI3
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
   
-  SPI3->CR1 |= SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_BIDIMODE | SPI_BaudRatePrescaler_32;
+  SPI3->CR1 |= SPI_CR1_LSBFIRST | SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_BIDIMODE | SPI_BaudRatePrescaler_32;
 
   //PC12 spi3 mosi
   //PC10 spi3 clock
@@ -56,28 +68,82 @@ static void nrt_init(void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   GPIO_SetBits(GPIOD, GPIO_Pin_10);  //clock tx enable
-
+  PIN(req) = 224;
+  PIN(pos_len) = 32;
 }
 
 static void rt_func(float period, void *ctx_ptr, hal_pin_inst_t *pin_ptr) {
   // struct endat_ctx_t *ctx      = (struct endat_ctx_t *)ctx_ptr;
   struct endat_pin_ctx_t *pins = (struct endat_pin_ctx_t *)pin_ptr;
 
+  SPI3->CR1 &= ~SPI_CR1_SPE;//disable spi
+
+  SPI3->CR1 |= SPI_CR1_LSBFIRST | SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_BIDIMODE | SPI_BaudRatePrescaler_32;
+
+
   GPIO_SetBits(GPIOD, GPIO_Pin_15);//tx enable
   SPI3->CR1 |= SPI_CR1_BIDIOE;//enable output
   SPI3->CR1 |= SPI_CR1_SPE;//enable spi
 
-  SPI3->DR = 0x07;
+  SPI3->DR = PIN(req);
   while(!(SPI3->SR & SPI_SR_TXE));
   while(SPI3->SR & SPI_SR_BSY);
   SPI3->CR1 &= ~SPI_CR1_BIDIOE;//disable output, this activates the clock
   GPIO_ResetBits(GPIOD, GPIO_Pin_15);//tx disable
   
-  volatile uint32_t foo = SPI3->DR;//dummy read to clear flags
-  for(int i = 0;i<6;i++){
+  SPI3->CR1 &= ~SPI_CR1_SPE;//disable spi
+  SPI3->CR1 |= SPI_CR1_LSBFIRST | SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_BIDIMODE | SPI_BaudRatePrescaler_32;
+  SPI3->CR1 |= SPI_CR1_SPE;//enable spi
+
+
+  //volatile uint32_t foo = SPI3->DR;//dummy read to clear flags
+  //for(int i = 0;i<6;i++){
+  //  while(!(SPI3->SR & SPI_SR_RXNE));
+  //  PINA(byte,i) = SPI3->DR;
+ // }
+
+union{
+    uint64_t data;
+    uint8_t dataa[8];
+  } df;
+
+  for(int i = 0; i < sizeof(df.data); i++){
     while(!(SPI3->SR & SPI_SR_RXNE));
-    PINA(byte,i) = SPI3->DR;
+    df.dataa[i] = SPI3->DR;
   }
+  df.data = df.data >> 4;
+  PIN(shift) = 4;
+  while((df.data & 1) == 0){
+    df.data = df.data >> 1;
+    PIN(shift)++;
+  }
+
+  uint8_t start_bit = (df.data & 1);
+  uint8_t f1_bit = (df.data & 2);
+  uint8_t f2_bit = (df.data & 3);
+  df.data = df.data >> 3;
+
+  uint32_t pos_len = PIN(pos_len);
+  uint32_t mpos_len = PIN(mpos_len);
+
+  uint32_t pos = df.data << (32 - pos_len);
+  uint8_t crc = df.data >> (pos_len + mpos_len);
+  uint32_t mpos = (df.data << (sizeof(df.data) * 8 - pos_len - mpos_len)) >> (64 - mpos_len);
+
+  PIN(pos) = mod(pos * 2.0 * M_PI / 4294967295.0);
+  PIN(mpos) = mpos;
+  //PIN(crc) = crc;
+  PIN(f1) = f1_bit;
+  PIN(f2) = f2_bit;
+
+  PINA(byte,0) = df.dataa[0];
+  PINA(byte,1) = df.dataa[1];
+  PINA(byte,2) = df.dataa[2];
+  PINA(byte,3) = df.dataa[3];
+  PINA(byte,4) = df.dataa[4];
+  PINA(byte,5) = df.dataa[5];
+
+
   //TODO: wait for busy flag?
   SPI3->CR1 &= ~SPI_CR1_SPE;//disable spi
 }
