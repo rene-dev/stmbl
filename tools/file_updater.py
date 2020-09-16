218726abc990e434d098062230200c8185ead646
Forest Darling <fdarling@gmail.com>
2020-09-16 14:54:14 -0500

    fixed Python file open mode

--- tools/file_updater.py
+++ tools/file_updater.py
@@ -6,7 +6,7 @@ import filecmp
 def copyIfDifferent(src, dst_path):
     dst = None
     try:
-        dst = open(dst_path, 'rw+b')
+        dst = open(dst_path, 'r+b')
         src.seek(0)
         if src.read() == dst.read():
             return
@@ -21,7 +21,7 @@ def copyIfDifferent(src, dst_path):
 class FileUpdater:
     def __init__(self, path):
         self.filePath = path
-        self.tempFile = tempfile.TemporaryFile(mode='rw+b')
+        self.tempFile = tempfile.TemporaryFile(mode='r+b')
 
     def __del__(self):
         if not self.tempFile.closed:
